d252f31de0faae61730a688b9c999376e96c2275
Adam Seewald <adam@seewald.email>
2022-08-03 18:39:29 -0400

    some updates to the interface

--- ground-based_master/tele2.php
+++ ground-based_master/tele2.php
@@ -28,6 +28,8 @@
             border: 1px solid #FF0000;
         } #joystick2 {
             border: 1px solid #0000FF;
+        } .slidecontainer {
+            width: 100%; /* Width of the outside container */
         }
     </style>
     <script src="../src/jquery-3.6.0.min.js"></script>
@@ -41,22 +43,42 @@
                 </div>
                 <div id="joy1Div" style="width:200px;height:200px;margin:50px;margin-top:5px;margin-bottom:5px;"></div>
                 <div style="width:200px;margin:50px;margin-top:5px;">
-
                     X :<input id="joy_x" type="text" /><br/>
                     Y :<input id="joy_y" type="text" /><br/>
                     DIR :<input id="joy_dir" type="text" />
+                    VEL :<input type="range" min="1" max="100" value="100" class="slider" id="vel" /><br/>
+                    FIX :<input type="checkbox" id="fix" />
             </div></div>
             <script type="text/javascript">
-                var Joy1 = new JoyStick('joy1Div', {}, function(stickData) {
-                     $.get("tele2_service.php?x=" + stickData.x + 
-                                            "&y=" + stickData.y,
-                           function(response) {}
-                          );
-                    document.getElementById("joy_x").value = stickData.x;
-                    document.getElementById("joy_y").value = stickData.y;
-                    document.getElementById("joy_dir").value = stickData.cardinalDirection;
-		});
-		var RGBNavCam1 = setInterval(function() { $.get("rgbnavcam.php", function(response) { document.getElementById('rgbnavcamImg').src = 'files/' + response }); }, 500);
+                function returnJoyStick(name, nfixed) {
+                    $('#'+name).empty();
+                    return new JoyStick(name, { "autoReturnToCenter": nfixed, "internalFillColor": "#0581ff", "externalStrokeColor": "#00356B", "internalStrokeColor": "#00356B" }, function(stickData) {
+                        var vel = document.getElementById("vel").value;
+                        $.get("tele2_service.php?x=" + Math.round(stickData.x*vel/100) + 
+                                               "&y=" + Math.round(stickData.y*vel/100),
+                              function(response) {}
+                             );
+                        document.getElementById("joy_x").value = Math.round(stickData.x*vel/100);
+                        document.getElementById("joy_y").value = Math.round(stickData.y*vel/100);
+                        document.getElementById("joy_dir").value = stickData.cardinalDirection;
+                    });
+                };
+                var Joy1 = returnJoyStick('joy1Div', true);
+
+		        var RGBNavCam1 = setInterval(function() { $.get("rgbnavcam.php", function(response) { 
+                    if (response=='..') { 
+                        document.getElementById('rgbnavcamImg').src = 'files/' + response;
+                    } else { 
+                        document.getElementById('rgbnavcamImg').src = '/src/empty.png';
+                    }
+                });}, 500);
+                $('#fix').click(function() {
+                    if ($(this).is(':checked')) {
+                        Joy1 = returnJoyStick('joy1Div', false);
+                    } else {
+                        location.reload(true);
+                    }
+                });
             </script>
         </div>
     </body>
